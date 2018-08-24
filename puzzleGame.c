2b89b361f3a1e0ef5da1fc50d6ca9db944e6184e
Miri The Wise <rampion@mindspring.com>
2018-08-23 21:04:45 -0500

    Initial upload
    Unfinished.

---
+++ puzzleGame.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*SEE ALGORITHM DESIGN
+Also, I think I need to use arrays of pointers to store the lines from the gamefile, because I will need to store large amounts of text. Then I can also compare strings easily. Use structs? Not sure. Look them up. 
+*/
+
+int main(){
+    //ints
+
+    //chars
+
+    //FILEs
+    FILE *gameFile;
+    FILE *notebook;
+    FILE *lockTried;
+    FILE *
+
+
+}
